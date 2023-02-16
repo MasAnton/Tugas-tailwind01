87032933c5400ec11343a33bde4b2e7cddcfb52a
MasAnton <antonprayogo002@gmail.com>
2023-02-16 23:46:57 +0700

    menambahkan all file

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        gelap: "#01141A",
+        biru: "#2FC3FF",
+        putih: "#FFFFFF",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: ["tailwindcss"],
+};
